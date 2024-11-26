fa317247128f92864ba06118213db678e5a53146
tchu2803 <tchu280305@gmail.com>
2024-11-26 16:32:40 +0700

    footer

--- view/footer.php
+++ view/footer.php
@@ -31,21 +31,33 @@
     }
 </script>
 <div>
+    <footer style="padding: 20px 0; text-align: center">
 
+
+
+
+        <div style="margin-bottom: 20px;">
+
+
+        </div>
+
+        <div style="margin-bottom: 20px;">
+        </div>
+
+
+        <div>
+            <p style="font-size: 20px; color: #777; text-align:center">2024 Tech Shop. All rights reserved.</p>
+        </div>
+</div>
+</footer>
 </div>
 
 </div>
 </body>
 
 </html>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<link rel="stylesheet" href="view/css/style.css">
+<link rel="icon" href="view/assets/img/icotittle.png" type="image/gif" sizes="16x16">
 <link rel="stylesheet" href="view/assets/css/main.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="view/assets/js/script.js"></script>
@@ -61,9 +73,9 @@
         <div class="footer-content-inner">
             <div class="grid wide">
                 <div class="row">
-                    <div class="col l-4 m-4 c-12" style="margin-bottom: 17px">
+                    <div class="col l-4 m-4 c-12" style="margin-bottom: 17px;">
                         <div class="footer__info">
-                         <div class="footer__info-brand">
+                            <div class="footer__info-brand">
                                 <img src="view/images/logo1.png" alt="" class="footer__info-brand-logo">
                             </div>
                             <div class="footer__info-brand-desc">
@@ -85,7 +97,17 @@
                     <div class="col l-4 m-4 c-12" style="margin-bottom:163px ; margin-right:50px">
                         <div class="row">
                             <div class="col l-6 m-6 c-6">
-
+                                <div class="footer__info">
+                                    <div class="footer__info-brand-desc">
+                                        TẢI VÀ CÀI ĐẶT ỨNG DỤNG TECHSHOP
+                                    </div>
+                                </div>
+                                <div class="footer_info-desc">
+                                    iOS
+                                    <img src="view/assets/img/appstore.png" alt="">
+                                    ANDROID
+                                    <img src="view/assets/img/chplay.png" alt="">
+                                </div>
                             </div>
                             <div class="col l-6 m-6 c-6" style="margin-left: 40px;margin-top:162px">
                                 <div class="footer__info">
@@ -101,7 +123,7 @@
                                         <br>hoặc
                                         <br>024 3902 8999
                                         <br>EMAIL
-                                        <br>contact@techshop.com
+                                        <br><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="b9dad6d7cdd8dacdf9cddcdad1cad1d6c997dad6d4">[email&#160;protected]</a>
                                     </p>
                                 </div>
                             </div>
@@ -126,16 +148,8 @@
             </div>
         </div>
     </div>
-    <div class="footer_info-desc" style="font-size: 40px; text-align: center;">
-    <span>iOS</span>
-    <img src="view/assets/img/appstore.png" alt="App Store" style="width: 150px; vertical-align: middle; margin: 0 5px;">
-    <span>ANDROID</span>
-    <img src="view/assets/img/chplay.png" alt="Google Play" style="width: 150px; vertical-align: middle; margin: 0 5px;">
-</div>
 </footer>
 </div>
 <div class="scrollTop">
     <i class="fas fa-chevron-up"></i>
-</div>
-</body>
-</html>
+</div><script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
